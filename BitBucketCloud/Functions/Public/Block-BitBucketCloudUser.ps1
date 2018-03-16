cf6cd3e471a45227b2a2fc35ca8481de3c64c818
Nicholas Dille <nicholasdille@users.noreply.github.com>
2018-03-16 09:13:39 +0100

    Should have been `$User`

--- BitBucketCloud/Functions/Public/Block-BitBucketCloudUser.ps1
+++ BitBucketCloud/Functions/Public/Block-BitBucketCloudUser.ps1
@@ -27,14 +27,14 @@ function Block-BitBucketCloudUser {
         }
 
         if ($Force -or $PSCmdlet.ShouldProcess("Remove user from all groups?")) {
-            $Groups = Get-BitBucketCloudGroup | Where-Object { $_.Members -contains 'zakagreenfield' }
+            $Groups = Get-BitBucketCloudGroup | Where-Object { $_.Members -contains $User }
             Foreach ($Group in $Groups) {
                 Remove-BitBucketCloudMember -Group $Group.Name -User $User
             }
         }
 
         if ($Force -or $PSCmdlet.ShouldProcess("Remove user from all repositories?")) {
-            $Privileges = Get-BitBucketCloudPrivilege | Where-Object { $_.User -like 'nicholasdille' }
+            $Privileges = Get-BitBucketCloudPrivilege | Where-Object { $_.User -like $User }
             Foreach ($Privilege in $Privileges) {
                 Remove-BitBucketCloudPrivilege -Repository $Privilege.Repository.Substring($Team.Length + 1) -User $User
             }
